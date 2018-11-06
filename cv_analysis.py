5018fb9ec3a8d21fe69a8c5deff22502e4228e97
jmpasquali <39780308+jmpasquali@users.noreply.github.com>
2018-11-06 19:40:30 -0200

    Add files via upload

--- cv_analysis.py
+++ cv_analysis.py
@@ -9,6 +9,7 @@ import numpy as np
 import csv
 import matplotlib.pyplot as plt
 import copy 
+from sklearn.linear_model import LinearRegression
 
 ## ----------------------funcs ----------------------
 def estimate_coef(x, y):
@@ -20,22 +21,23 @@ def estimate_coef(x, y):
 	m_x, m_y = np.mean(x), np.mean(y)
 
 	## Calculating cross-deviation and deviation about x
-	SS_xy = np.sum(y*x - n*m_y*m_x)
+	SS_xy = np.sum(y*x - n * m_y * m_x)
 	SS_xx = np.sum(x*x - n*m_x*m_x)
 
 	## Calculate regression coefficients
 	b_1 = SS_xy / SS_xx
-	b_0 = m_y - b_1*m_x
+	b_0 = m_y - b_1 * m_x
 
 	return(b_0, b_1)
 
-def plot_regression_line(x, b):
+def plot_regression_line(x, y, b):
 
 	# predicted response vector
 	y_pred = b[0] + b[1]*x
 
 	# plotting the regression line
 	plt.plot(x, y_pred, color = "m")
+	#plt.plot(x, y, 'ro')
 
 def find_slope_idx(x, y, dy):
 
@@ -55,21 +57,19 @@ def find_slope_idx(x, y, dy):
 			soma = soma + dy[i+k]
 		ma50.append(round(soma/50, 3))
 		
-	plt.plot(x[0:len(ma50)], ma50, x[0:len(ma20)], ma20)#, y1, x2, y2, x1, dy1, x2, dy2)#, x[0:len(ma50)], ma50)
+	## Plot moving averages
+	plt.plot(x[0:len(ma50)], ma50, x[0:len(ma20)], ma20)
+		
 	## Find intercepts of movingAverage curves
-
 	ma20_num = np.array(ma20)
 	ma50_num = np.array(ma50)
 	idx = np.argwhere(np.diff(np.sign(ma50_num - ma20_num[:len(ma50)]))!= 0).reshape(-1)+0
-	print('idx ', idx)
+	
+	## Plot intersections 
 	plt.plot(x[idx], ma20_num[idx], 'ro')
- 
-	## Linear regression of data untill critical x
-	b = estimate_coef(np.array(x[0:idx[0]]), np.array(y[0:idx[0]]))
-	print('coefs ', b)
-	plot_regression_line(x, b)
 	
 	return idx
+
 ##-----------------------main-------------------------
 
 ## Provide list of the names of the files to be processed  
@@ -88,6 +88,7 @@ for filename in fileNames:
 	y = pd.to_numeric(data['I /uA.1'])
 	dy = np.diff(y)/0.0035 #(np.diff(x1))
 	
+	## Split Vectors (still needs some work)
 	x_copy = np.array(x)
 	y_copy = np.array(y)
 	dy_copy = np.array(dy)
@@ -101,22 +102,32 @@ for filename in fileNames:
 	dy2 = dy_copy[(half+1):end]
 	x2= x_copy[(half+1):end]
 	y2 = y_copy[(half+1):end]
-
-	plt.plot(x1, y1, x2, y2, x1, dy1, x2, dy2)#, x[0:len(ma50)], ma50)
+	
+	## Plot all data 
+	plt.plot(x1, y1, x2, y2, x1, dy1, x2, dy2)
 	plt.xlabel('Eixo X')
 	plt.ylabel('Eixo Y')
-	find_slope_idx(x1, y1, dy1)
-	#find_slope_idx(x2, y2, dy2)
+
+
+	## Plot linear regression of 
+	idx1 = find_slope_idx(x1, y1, dy1)
+	b1 = estimate_coef(np.array(x1[0:idx1[0]]), np.array(y1[0:idx1[0]]))
+	plot_regression_line(x1, y1, b1)
 	
-	idx = 	find_slope_idx(x2, y2, dy2)
-	print(idx)
-	## Linear regression of data untill critical x
-	b = estimate_coef(x[510:524], y[510:524])
-	print('coefs ', b)
-	plot_regression_line(x2, b)
+	## Not quite working yet 
+	#idx2 = find_slope_idx(x2, y2, dy2)
+	#b2 = estimate_coef(np.array(x2[0:idx2[0]]),(np.array(y2[0:idx2[0]])))
+	#plot_regression_line(np.array(x2[0:idx2[0]]),np.array(y2[0:idx2[0]]),b2)
 	
+	min_pos = y1.argmin() 
+	max_pos = y2.argmax()
 	
-	print(x1)
-	print(x2)
+	print(min_pos, max_pos)
+	plt.plot( x1[min_pos], y1[min_pos], 'bo')
+	plt.plot( x2[max_pos], y2[max_pos], 'bo')
+	
 	plt.savefig(filename + '_saida.png')
 	plt.show()
+		
+		
+		 
