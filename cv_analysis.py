f02264ed5b00ffd9a2ca0d953946b9ce2167fbb9
jmpasquali <39780308+jmpasquali@users.noreply.github.com>
2018-11-07 19:18:18 -0200

    Add files via upload

--- cv_analysis.py
+++ cv_analysis.py
@@ -8,8 +8,6 @@ import pandas as pd
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
-import copy 
-from sklearn.linear_model import LinearRegression
 
 def media(vetor):
         soma = 0
@@ -17,13 +15,11 @@ def media(vetor):
             soma = soma + x
         return soma / len(vetor)    
 
-
 def soma(vetor):
         soma = 0
         for x in vetor:
             soma = soma + x
         return soma
-
 
 def multiplica(vetor_x, vetor_y):
         soma = 0
@@ -31,20 +27,19 @@ def multiplica(vetor_x, vetor_y):
             soma = soma + x * y
         return soma
         
-
 def find_coeffs(x, y):
   
+  	## Inclination coeff
         soma_x = soma(x)
         soma_x2 = multiplica(x,x) 
         soma_xy = multiplica(x,y) 
         media_x = media(x)
         media_y = media(y)
-        
         m = (soma_xy - soma_x * media_y) / (soma_x2 - soma_x * media_x)
         
+        ## Y axis intercept coeff
         media_x = media(x)
         media_y = media(y)
-        
         b = media_y - m * media_x
         
         return m, b
@@ -58,8 +53,10 @@ def y_predict(m, b, x):
         
     return new_y    
 
-def find_slope_idx(x, y, dy):
-
+def finds_critical_idx(x, y): ## Finds index where data set is no longer linear 
+	
+	dy = np.diff(y)/(np.diff(x))
+	
 	## Calculate moving average for 20 and 50 points
 	ma20 = []
 	ma50 = []
@@ -81,23 +78,46 @@ def find_slope_idx(x, y, dy):
 	ma50_num = np.array(ma50)
 	idx = np.argwhere(np.diff(np.sign(ma50_num - ma20_num[:len(ma50)]))!= 0).reshape(-1)+0
 	
-	## Uncomment to see whats going on 
+	## Uncomment to check whats going on 
 	'''plt.plot(x[idx], ma20_num[idx], 'ro') ## Plot intersections 
 	plt.plot(x[0:len(ma50)], ma50, x[0:len(ma20)], ma20) ## Plot moving averages'''
 
 	return int(idx[0])
 
-def plot_lines(x, y, dy):
+def calc_linear_background(x, y):
 
-	idx = find_slope_idx(x, y, dy) + 25
+	idx = finds_critical_idx(x, y) + 25
 	half_idx = int(0.5 * idx)
 	m, b = find_coeffs(x[(idx - half_idx) : (idx + half_idx)], y[(idx - half_idx) : (idx + half_idx)])
 	y_pred = y_predict(m, b, x)
-	plt.plot(x, y_pred, color = "fuchsia",linestyle=':',linewidth=2) #orangered ##
 	
 	return y_pred
+
+def graph_format(filename):
+
+	plt.xlabel(' E [V]' )
+	plt.ylabel(' I [uA] ')
+	plt.title(filename)
+	plt.grid(color='0', linewidth=0.1)
+	ax = plt.subplot(111)    
+	ax.spines["top"].set_visible(False)    
+#	ax.spines["bottom"].set_visible(False)    
+	ax.spines["right"].set_visible(False)    
+#	ax.spines["left"].set_visible(False) 
+#	plt.tick_params(axis="both", which="both", bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on")   
+	plt.xticks(fontsize=8)  
+	plt.yticks(fontsize=8)
 	
+def split_vector(vector):
+
+	half = int(len(vector)/2)
+	end = int(len(vector))
+	vector_copy = np.array(vector)
+	vector1 = vector_copy[0:half]
+	vector2 = vector_copy[(half+1):end]
 	
+	return vector1, vector2
+
 def main():
 	
 	input =  raw_input('Name of csv containing file names list: ')
@@ -117,69 +137,44 @@ def main():
 	out_data ={}
 	for filename in inputList:
 
+		## Use the right separator and decimal for your data 
 		data = pd.read_csv(filename, sep='\t',decimal=",")
-
-		## Differenciante data 
-		x = pd.to_numeric(data['E /V'])
+		x = pd.to_numeric(data['E /V']) 
 		y = pd.to_numeric(data['I /uA.1'])
-		dy = np.diff(y)/(np.diff(x)) # 0.0035 
 	
-		## Split Vectors (still needs some work)
-		x_copy = np.array(x)
-		y_copy = np.array(y)
-		dy_copy = np.array(dy)
-	
-		half = int(len(dy)/2)
-		end = int(len(dy))
-	
-		dy1 = dy_copy[0:half]
-		x1 = x_copy[0:half]
-		y1 = y_copy[0:half]
-	
-		dy2 = dy_copy[(half+1):end]
-		x2= x_copy[(half+1):end]
-		y2 = y_copy[(half+1):end]
-	
-		## Plot all data 
-		plt.plot(x1, y1,'dodgerblue',linewidth=1) ##darkgrey
-		plt.plot(x2, y2,'dodgerblue', linewidth=1) ##dodgerblue
-		plt.xlabel(' E [V]' )
-		plt.ylabel(' I [uA] ')
-		plt.title(filename)
-		plt.grid(color='0', linewidth=0.1)
-		ax = plt.subplot(111)    
-		ax.spines["top"].set_visible(False)    
-	#	ax.spines["bottom"].set_visible(False)    
-		ax.spines["right"].set_visible(False)    
-	#	ax.spines["left"].set_visible(False) 
-	#	plt.tick_params(axis="both", which="both", bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on")   
-		plt.xticks(fontsize=8)  
-		plt.yticks(fontsize=8)  
-
+		## Split vectors
+		x1, x2 = split_vector(x)
+		y1, y2 = split_vector(y)
 		
-		## Find linear regression and predicted line	
-		y_pred1 = plot_lines(x1,y1, dy1)
-		y_pred2 = plot_lines(x2,y2, dy2)
+		## Finds linear background data	
+		y_pred1 = calc_linear_background(x1,y1)
+		y_pred2 = calc_linear_background(x2,y2)
 	
-		## Finds min and max peaks
-		min_pos = y1.argmin() 
+		## Cathodic peak current and potential 
 		max_pos = y2.argmax()
-	
-		#plt.plot( x1[min_pos], y1[min_pos],'orange',marker='o', markersize=5)
-		#plt.plot( x2[max_pos], y2[max_pos], 'orange',marker='o', markersize=5)
-	
 		max_pot = x2[max_pos]
 		max_cur = y2[max_pos] - y_pred1[max_pos]
+		
+		## Anodic peak current and potential 
+		min_pos = y1.argmin() 
 		min_pot = x1[min_pos]
 		min_cur = y1[min_pos] - y_pred2[min_pos]
 	
-		plt.savefig(filename + '_saida.png')
-		#plt.show()
-		#out_data[] = {}
-		out_data[filename] = {'Ipc': max_cur , 'Vpc': max_pot,'Ipa': min_cur, 'Vpa': min_pot}
+		## Uncomment to check peaks position
+		'''plt.plot( x1[min_pos], y1[min_pos],'orange',marker='o', markersize=5)
+		plt.plot( x2[max_pos], y2[max_pos], 'orange',marker='o', markersize=5)'''
+		
+		## Plot all data 
+		plt.plot(x1, y1,'dodgerblue',linewidth=1) ##darkgrey
+		plt.plot(x2, y2,'dodgerblue', linewidth=1) ##dodgerblue  
+		plt.plot(x1, y_pred1, color = "fuchsia", linestyle=':',linewidth=2) #orangered 
+		plt.plot(x2, y_pred2, color = "fuchsia", linestyle=':',linewidth=2) #orangered 
+		graph_format(filename)
+		plt.savefig(filename + '_output.png')
+		print '\nCreating plot...',
 		plt.clf()
-
 	
+	## Writes to file
 	print '\nWriting to file...',
 	myfile = open('output.txt', 'w')
 	myfile.write('FileName\tIpc [ uA ]\tVpc [ V ]\tIpa [ uA ]\tVpa [ V ]\n')
