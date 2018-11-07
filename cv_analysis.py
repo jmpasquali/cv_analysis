8c00223ee7cf6cee5939995d94cbd6e02b8403e9
jmpasquali <39780308+jmpasquali@users.noreply.github.com>
2018-11-07 17:10:01 -0200

    Add files via upload

--- cv_analysis.py
+++ cv_analysis.py
@@ -8,12 +8,15 @@ import pandas as pd
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
+import copy 
+from sklearn.linear_model import LinearRegression
 
 def media(vetor):
         soma = 0
         for x in vetor:
             soma = soma + x
-        return soma / len(vetor)   
+        return soma / len(vetor)    
+
 
 def soma(vetor):
         soma = 0
@@ -21,11 +24,13 @@ def soma(vetor):
             soma = soma + x
         return soma
 
+
 def multiplica(vetor_x, vetor_y):
         soma = 0
         for x,y in zip(vetor_x, vetor_y):
             soma = soma + x * y
         return soma
+        
 
 def find_coeffs(x, y):
   
@@ -88,17 +93,29 @@ def plot_lines(x, y, dy):
 	half_idx = int(0.5 * idx)
 	m, b = find_coeffs(x[(idx - half_idx) : (idx + half_idx)], y[(idx - half_idx) : (idx + half_idx)])
 	y_pred = y_predict(m, b, x)
-	plt.plot(x, y_pred, color = "m")
+	plt.plot(x, y_pred, color = "fuchsia",linestyle=':',linewidth=2) #orangered ##
 	
 	return y_pred
 	
+	
 def main():
+	
+	input =  raw_input('Name of csv containing file names list: ')
+	
+	## List of the names of the files needs to be provided
+	fileNames = [input]#,'SPE_Gaiola_2.txt','PadraoK3_Gaiola_cell3eletrodos.txt']
 
-	## File names list needs to be provided
-	fileNames = ['SPE_FC_3.txt','SPE_Gaiola_2.txt','PadraoK3_Gaiola_cell3eletrodos.txt']
-
+	# Import the files names from the file Nomes.csv
+	inputList = []
+	with open(input,'rb') as csvfile:
+	    spamreader = csv.reader(csvfile, delimiter=',', quotechar = '|')
+	    for row in spamreader:
+		name = str(row[0])
+		inputList.append(name)
+             
 	## Import file to pandas dataFrame
-	for filename in fileNames:
+	out_data ={}
+	for filename in inputList:
 
 		data = pd.read_csv(filename, sep='\t',decimal=",")
 
@@ -124,10 +141,22 @@ def main():
 		y2 = y_copy[(half+1):end]
 	
 		## Plot all data 
-		plt.plot(x1, y1, x2, y2)
-		plt.xlabel('Eixo X')
-		plt.ylabel('Eixo Y')
+		plt.plot(x1, y1,'dodgerblue',linewidth=1) ##darkgrey
+		plt.plot(x2, y2,'dodgerblue', linewidth=1) ##dodgerblue
+		plt.xlabel(' E [V]' )
+		plt.ylabel(' I [uA] ')
+		plt.title(filename)
+		plt.grid(color='0', linewidth=0.1)
+		ax = plt.subplot(111)    
+		ax.spines["top"].set_visible(False)    
+	#	ax.spines["bottom"].set_visible(False)    
+		ax.spines["right"].set_visible(False)    
+	#	ax.spines["left"].set_visible(False) 
+	#	plt.tick_params(axis="both", which="both", bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on")   
+		plt.xticks(fontsize=8)  
+		plt.yticks(fontsize=8)  
 
+		
 		## Find linear regression and predicted line	
 		y_pred1 = plot_lines(x1,y1, dy1)
 		y_pred2 = plot_lines(x2,y2, dy2)
@@ -136,18 +165,30 @@ def main():
 		min_pos = y1.argmin() 
 		max_pos = y2.argmax()
 	
-		plt.plot( x1[min_pos], y1[min_pos], 'bo')
-		plt.plot( x2[max_pos], y2[max_pos], 'bo')
+		#plt.plot( x1[min_pos], y1[min_pos],'orange',marker='o', markersize=5)
+		#plt.plot( x2[max_pos], y2[max_pos], 'orange',marker='o', markersize=5)
 	
 		max_pot = x2[max_pos]
 		max_cur = y2[max_pos] - y_pred1[max_pos]
 		min_pot = x1[min_pos]
 		min_cur = y1[min_pos] - y_pred2[min_pos]
 	
-		print max_cur, max_pot, min_cur, min_pot
-	
 		plt.savefig(filename + '_saida.png')
-		plt.show()
+		#plt.show()
+		#out_data[] = {}
+		out_data[filename] = {'Ipc': max_cur , 'Vpc': max_pot,'Ipa': min_cur, 'Vpa': min_pot}
+		plt.clf()
+
+	
+	print '\nWriting to file...',
+	myfile = open('output.txt', 'w')
+	myfile.write('FileName\tIpc [ uA ]\tVpc [ V ]\tIpa [ uA ]\tVpa [ V ]\n')
+
+	for name in out_data:
+	    myString = "%s\t%.2f\t%.2f\t%.2f\t%.2f\n" % (name, out_data[name]['Ipc'], out_data[name]['Vpc'],out_data[name]['Ipa'], out_data[name]['Vpa'])
+	    myfile.write(str(myString))
+	     
+	print 'Done!'
 
 if __name__ == "__main__":
     main()
