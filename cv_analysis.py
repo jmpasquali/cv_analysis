21ca5d688b4224db763b782bac97ca4f7d6e225d
jmpasquali <39780308+jmpasquali@users.noreply.github.com>
2018-11-09 16:59:49 -0200

    Add files via upload

--- cv_analysis.py
+++ cv_analysis.py
@@ -95,19 +95,20 @@ def calc_linear_background(x, y):
 
 def graph_format(filename):
 
-	plt.xlabel(' E [V]' )
-	plt.ylabel(' I [uA] ')
+	plt.xlabel(' E [ V ]' )
+	plt.ylabel(' I [ uA ] ')
 	plt.title(filename)
 	plt.grid(color='0', linewidth=0.1)
-	ax = plt.subplot(111)    
-	ax.spines["top"].set_visible(False)    
+	ax = plt.subplot(111)#111)    
+	ax.spines["top"].set_visible(False)   
 #	ax.spines["bottom"].set_visible(False)    
-	ax.spines["right"].set_visible(False)    
+	ax.spines["right"].set_visible(False) 
 #	ax.spines["left"].set_visible(False) 
 #	plt.tick_params(axis="both", which="both", bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on")   
 	plt.xticks(fontsize=8)  
 	plt.yticks(fontsize=8)
-	
+	    	
+	    
 def split_vector(vector):
 
 	half = int(len(vector)/2)
@@ -120,62 +121,87 @@ def split_vector(vector):
 
 def main():
 	
-	input =  raw_input('Name of csv containing file names list: ')
-	
-	## List of the names of the files needs to be provided
-	fileNames = [input]#,'SPE_Gaiola_2.txt','PadraoK3_Gaiola_cell3eletrodos.txt']
-
-	# Import the files names from the file Nomes.csv
-	inputList = []
-	with open(input,'rb') as csvfile:
-	    spamreader = csv.reader(csvfile, delimiter=',', quotechar = '|')
-	    for row in spamreader:
-		name = str(row[0])
-		inputList.append(name)
-             
+	validFileName = 0
+	while(validFileName != 1):
+		try:
+			input =  raw_input('Name of txt containing file names list: ')
+			# Import the files names from the input file 
+			inputList = []
+			with open(input,'rb') as csvfile:
+			    spamreader = csv.reader(csvfile, delimiter=',', quotechar = '|')
+			    for row in spamreader:
+				name = str(row[0])
+				inputList.append(name)
+			validFileName = 1
+		except:
+			print 'Invalid input file ' + input 
+		
 	## Import file to pandas dataFrame
 	out_data ={}
+	validLabels = 0
+	
 	for filename in inputList:
 
-		## Use the right separator and decimal for your data 
-		data = pd.read_csv(filename, sep='\t',decimal=",")
-		x = pd.to_numeric(data['E /V']) 
-		y = pd.to_numeric(data['I /uA.1'])
-	
-		## Split vectors
-		x1, x2 = split_vector(x)
-		y1, y2 = split_vector(y)
+		validFileName = 1
+		## Might have to change the separator and decimal for your data 
+		try:
+			data = pd.read_csv(filename, sep='\t',decimal=",")
+		except: 
+			print '\nInvalid file ' + filename + '!!!',
+			validFileName = 0
+			
+		if (validFileName == 1):
+			
+			while(validLabels != 1):
+				x_label = raw_input('X data column label: ')
+				y_label =  raw_input('Y data column label: ')
+				data = pd.read_csv(filename, sep='\t',decimal=",")
+				headers = data.dtypes.index
+				if ((x_label in headers) and (y_label in headers)):
+					validLabels = 1
+				else:
+					print 'Invalid label '+x_label+' or '+y_label+' !!!\n',
+			
+			#x = pd.to_numeric(data['E /V']) 
+			#y = pd.to_numeric(data['I /uA.1'])
+			x = pd.to_numeric(data[x_label]) 
+			y = pd.to_numeric(data[y_label])
 		
-		## Finds linear background data	
-		y_pred1 = calc_linear_background(x1,y1)
-		y_pred2 = calc_linear_background(x2,y2)
-	
-		## Cathodic peak current and potential 
-		max_pos = y2.argmax()
-		max_pot = x2[max_pos]
-		max_cur = y2[max_pos] - y_pred1[max_pos]
+			## Split vectors
+			x1, x2 = split_vector(x)
+			y1, y2 = split_vector(y)
 		
-		## Anodic peak current and potential 
-		min_pos = y1.argmin() 
-		min_pot = x1[min_pos]
-		min_cur = y1[min_pos] - y_pred2[min_pos]
+			## Finds linear background 	
+			y_pred1 = calc_linear_background(x1,y1)
+			y_pred2 = calc_linear_background(x2,y2)
 	
-		## Uncomment to check peaks position
-		'''plt.plot( x1[min_pos], y1[min_pos],'orange',marker='o', markersize=5)
-		plt.plot( x2[max_pos], y2[max_pos], 'orange',marker='o', markersize=5)'''
+			## Cathodic peak current and potential 
+			max_pos = y2.argmax()
+			max_pot = x2[max_pos]
+			max_cur = y2[max_pos] - y_pred1[max_pos]
 		
-		## Plot all data 
-		plt.plot(x1, y1,'dodgerblue',linewidth=1) ##darkgrey
-		plt.plot(x2, y2,'dodgerblue', linewidth=1) ##dodgerblue  
-		plt.plot(x1, y_pred1, color = "fuchsia", linestyle=':',linewidth=2) #orangered 
-		plt.plot(x2, y_pred2, color = "fuchsia", linestyle=':',linewidth=2) #orangered 
-		graph_format(filename)
-		plt.savefig(filename + '_output.png')
-		print '\nCreating plot...',
-		plt.clf()
+			## Anodic peak current and potential 
+			min_pos = y1.argmin() 
+			min_pot = x1[min_pos]
+			min_cur = y1[min_pos] - y_pred2[min_pos]
+			out_data[filename] = {'Ipc': max_cur , 'Vpc': max_pot,'Ipa': min_cur, 'Vpa': min_pot}
+		
+			## Uncomment to check peaks position
+			'''plt.plot( x1[min_pos], y1[min_pos],'orange',marker='o', markersize=5)
+			plt.plot( x2[max_pos], y2[max_pos], 'orange',marker='o', markersize=5)'''
+		
+			## Plot all data 
+			plt.plot(x1, y1,'dodgerblue',linewidth=1) ##darkgrey
+			plt.plot(x2, y2,'dodgerblue', linewidth=1) ##dodgerblue  
+			plt.plot(x1, y_pred1, color = "orangered", linestyle=':',linewidth=2) #orangered 
+			plt.plot(x2, y_pred2, color = "orangered", linestyle=':',linewidth=2) #orangered 
+			graph_format(filename)
+			plt.savefig(filename + '_output.png')
+			print '\nCreating plot for ' + filename + '...',
+			plt.clf()
 	
 	## Writes to file
-	print '\nWriting to file...',
+	print '\nWriting data to file...',
 	myfile = open('output.txt', 'w')
 	myfile.write('FileName\tIpc [ uA ]\tVpc [ V ]\tIpa [ uA ]\tVpa [ V ]\n')
 
